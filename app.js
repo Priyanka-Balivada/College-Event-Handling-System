05e37e2adbfd4481229cfd20dcd4ae1ce41d330c
Priyanka <balivadapriyanka2519@gmail.com>
2022-05-25 18:37:09 +0530

    Student Representatives upload and display done

--- app.js
+++ app.js
@@ -8,9 +8,6 @@ const bodyParser = require("body-parser");
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine','ejs');
-//app.use(express.static(__dirname + '/public'));
-//app.use(express.json());
-//app.use(express.static(path.join("public")));
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({
   extended: true
@@ -20,18 +17,8 @@ app.use(bodyParser.urlencoded({
 // database
 mongoose.connect("mongodb://localhost:27017/EventDB");
 
+
 //schemas
-
-// var mediaSchema = new mongoose.Schema({
-//     name: String,
-//     desc: String,
-//     img:
-//     {
-//         data: Buffer,
-//         contentType: String
-//     }
-// });
-
 
 const mediaSchema=new mongoose.Schema({
   filename:{
@@ -49,10 +36,46 @@ const mediaSchema=new mongoose.Schema({
   }
 });
 
+const studentRespresentativeSchema=new mongoose.Schema({
+  filename:{
+    type:String,
+    unique:true,
+    required:true
+  },
+  contentType:{
+    type:String,
+    required:true
+  },
+  imageBase64:{
+    type:String,
+    required:true
+  },
+  name:{
+    type:String,
+    required:true
+  },
+  post:{
+    type:String,
+    required:true
+  },
+  email:{
+    type:String,
+    required:true
+  },
+  mobile:{
+    type:Number,
+    required:true
+  }
+})
+
 //models
 const Media = mongoose.model('media', mediaSchema);
+const StudentRespresentative = mongoose.model('studentRespresentative',studentRespresentativeSchema);
 
 //routes
+
+// ************************** Event Gallery *********************************
+
 app.get("/",function(req,res){
   Media.find({},function(err, items){
  		if (err) {
@@ -115,19 +138,6 @@ app.post("/UploadMedia",store.array('medias',20),function(req,res,next){
 
 
 app.get("/DeleteMedia",function(req,res){
- // Media.find({},function(err, items){
-	// 	if (err) {
-	// 		console.log(err);
-	// 		res.status(500).send('An error occurred', err);
-	// 	}
-	// 	else {
-	// 		res.render('displayMedia', { items: items });
-	// 	}
-	// })
- //  .clone().catch(function(err){ console.log(err)})
-
- //const items = Media.find({});
-
  Media.find({},function(err, items){
 		if (err) {
 			console.log(err);
@@ -156,11 +166,70 @@ app.post("/DeleteMedia",function(req,res){
 });
 
 
+// ********************************** Student Representatives ***************************
+
+app.get("/UploadStudentRepresentatives",function(req,res){
+  res.render("uploadStudentRepresentatives");
+});
+
+app.post("/UploadStudentRepresentatives",store.array('pics',1),function(req,res,next){
+  const files=req.files;
+
+  if(!files){
+    const error=new Error('Please choose files');
+    error.httpStatusCode=400;
+    return next(error);
+  }
+
+  //convert images into base64 encoding
+
+  let imgArray=files.map(function(file){
+    let img=fs.readFileSync(file.path)
+
+    return encode_image=img.toString('base64');
+  })
+
+  let result=imgArray.map(function(src,index){
+    //create object to store media into the collection
+    var images={
+      filename:files[index].originalname,
+      contentType:files[index].mimetype,
+      imageBase64:src,
+      name:req.body.name,
+      post:req.body.post,
+      email:req.body.email,
+      mobile:req.body.mobile
+    }
+
+    let studentRespresentative=new StudentRespresentative(images);
+
+    studentRespresentative.save(function(err,media) {
+      if (!err) {
+        console.log("Upload Successful");
+      }
+      else{
+        console.log("Upload Unsuccessful");
+      }
+    });
+
+  })
+
+  res.redirect("/DisplayStudentRespresentative");
+
+});
 
 
-
-
-
+app.get("/DisplayStudentRespresentative",function(req,res){
+  StudentRespresentative.find({},function(err, items){
+ 		if (err) {
+ 			console.log(err);
+ 			res.status(500).send('An error occurred', err);
+ 		}
+ 		else {
+ 			res.render('coordinators',{ items: items });
+ 		}
+ 	})
+});
 
 
 
